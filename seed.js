ffdc84810e2d5fefc884282fec72945a4750d2b0
Daniel Yu <daniel2004yu@gmail.com>
2023-02-19 21:42:01 -0500

    reset DB

--- seed.js
+++ seed.js
@@ -1,37 +1,38 @@
+/* eslint-disable */
 const sequelize = require('./database');
 const { Hacker, Skill } = require('./models');
 const data = require('./seedData.json');
 
 // Inserts user and their skills into DB
-const seedUser = async (user) => {
+const seedUser = (user) => new Promise(async (resolve, reject) => {
   try {
     const hacker = await Hacker.create(user);
 
-    await Promise.all(user.skills.map(async (newSkill) => { // TODO: DRY
+    for (let i = 0; i < user.skills.length; i++) {
       let skill = await Skill.findOne({
-        where: { skillName: newSkill.skill },
+        where: { skillName: user.skills[i].skill },
       });
 
       // If skill doesn't exist, create it
       if (!skill) {
         skill = await Skill.create({
-          skillName: newSkill.skill,
+          skillName: user.skills[i].skill,
         });
       }
-
-      await hacker.addSkill(skill, { through: { rating: newSkill.rating } });
-    }));
+      await hacker.addSkill(skill, { through: { rating: user.skills[i].rating } });
+      resolve();
+    }
   } catch (err) {
     console.error(err);
   }
-};
+});
 
 // Seeds all users from JSON
 const seed = () => new Promise(async (resolve, reject) => {
   try {
-    await Promise.all(data.map(async (user) => {
-      await seedUser(user);
-    }));
+    for (let i = 0; i < data.length; i++) {
+      await seedUser(data[i]);
+    }
 
     resolve();
   } catch (err) {
